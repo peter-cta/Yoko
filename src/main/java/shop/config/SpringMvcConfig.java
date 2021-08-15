9d894459ca5ba0f066c6fd48566355bae815e9af
Peter99-AI <peter.cta99@gmail.com>
2021-08-16 02:38:51 +0700

    Done

---
+++ src/main/java/shop/config/SpringMvcConfig.java
@@ -0,0 +1,82 @@
+package shop.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import shop.dao.AccountDAO;
+import shop.dao.AccountDAOImpl;
+import shop.dao.BrandDAO;
+import shop.dao.BrandDAOImpl;
+import shop.dao.CustomerDAO;
+import shop.dao.CustomerDAOImpl;
+import shop.dao.ProductDAO;
+import shop.dao.ProductDAOImpl;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "shop")
+public class SpringMvcConfig implements WebMvcConfigurer {
+	
+	
+	@Bean
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource  = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/YokoShop");
+		dataSource.setUsername("root");
+		dataSource.setPassword("chuanh255");
+		return dataSource;
+	}
+	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+          .addResourceHandler("/resources/**")
+          .addResourceLocations("/resources/");	
+    }
+	
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		
+		return resolver;
+	}
+	
+	
+	@Bean
+	public AccountDAO getAccountDAO() {
+		AccountDAO dao = new AccountDAOImpl(getDataSource());
+		return dao;
+	}
+	
+	@Bean
+	public BrandDAO getBrandDAO() {
+		BrandDAO dao = new BrandDAOImpl(getDataSource());
+		return dao;
+	}
+	
+	@Bean
+	public CustomerDAO getCustomerDAO() {
+		CustomerDAO dao = new CustomerDAOImpl(getDataSource());
+		return dao;
+	}
+	
+	@Bean
+	public ProductDAO getProductDAO() {
+		ProductDAO dao = new ProductDAOImpl(getDataSource());
+		return dao;
+	}
+	
+}
---
+++ src/main/java/shop/config/WebAppInitializer.java
@@ -0,0 +1,37 @@
+package shop.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+@EnableWebMvc
+public class WebAppInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+		appContext.register(SpringMvcConfig.class);
+		
+		ServletRegistration.Dynamic dispatcher 
+			= servletContext.addServlet("SpringDispatcher", new DispatcherServlet(appContext));
+		
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("/");
+		
+	}
+	
+
+	
+
+
+}
